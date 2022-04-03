41b2bd23a6a841ac76731bd476570a3be19936a9
yograjopcito <yograjopcito@opcito.com>
2022-04-03 22:30:34 +0530

    guest user seed data

--- seed.py
+++ seed.py
@@ -39,14 +39,18 @@ def sla_config_details():
 
 def sla_user_management():
     db.session.add(SlaUserManagement('Admin User','admin', 'admin@abc.com', 'pbkdf2:sha256:150000$R9C4k2s5$c3fcffa2aa15dcfaaf260bb9d693f259b8559e72150fe3c83249799755ea23bf',0,0,0))
+    db.session.add(SlaUserManagement('Guest User', 'guest', 'akshaysoni460@gmail.com',
+                                     "pbkdf2:sha256:150000$OlqR2npc$4f3264f113f2c62d8610bb0801b830ff0db4f1494bb06b1d5531ce9aa29f0fb0",
+                                     0, 0, 0))
 
 
 def sla_user_role():
-    db.session.add(SlaUserRole(user_id="admin",account='maxico',role='admin'))
+    db.session.add(SlaUserRole(user_id="admin",account='maxico',role='manager'))
+    db.session.add(SlaUserRole(user_id="guest", account='maxico', role='default'))
 
 
 def seed_sla_data():
-    db.session.add(SlaDataModel(account='maxico',application="python",sla_date=date.today(),sla_number=12,sla_type='09_AM',total_reqeust=2000,within_sla_reqeust=1600,sla_percentage=70,sla_met=True))
+    db.session.add(SlaDataModel(account='maxico',application="python",sla_date=date.today(),sla_number=12,sla_type='monthly',total_reqeust=2000,within_sla_reqeust=1600,sla_percentage=70,sla_met=True))
 
 
 def remaing():
