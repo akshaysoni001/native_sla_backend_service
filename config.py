6f7b18fa9b7972f69bb606e7ee1cec1b66285dc2
akshaysoni001 <akshaysoni460@gmail.com>
2022-04-04 08:48:18 +0530

    Proc file added

--- config.py
+++ config.py
@@ -13,7 +13,25 @@ class Config(object):
 
 
 class DevelopmentConfig(Config):
-    load_dotenv(path.join(basedir, 'dev.env'))
+    # load_dotenv(path.join(basedir, 'dev.env'))
+    # MYSQL_HOST = environ["MYSQL_HOST"]
+    # MYSQL_USER_NAME = environ["MYSQL_USER_NAME"]
+    # MYSQL_PASSWORD = environ["MYSQL_PASSWORD"]
+    # MYSQL_DATABASE_NAME = environ["MYSQL_DATABASE_NAME"]
+    # SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{MYSQL_USER_NAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DATABASE_NAME}"
+    # MAIL_SERVER = environ["MAIL_SERVER"]
+    # MAIL_PORT = environ["MAIL_PORT"]
+    # MAIL_USERNAME = environ["MAIL_USERNAME"]
+    # MAIL_PASSWORD = environ["MAIL_PASSWORD"]
+    # MAIL_DEFAULT_SENDER = environ["MAIL_DEFAULT_SENDER"]
+    # MAIL_USE_TLS = environ.get('MAIL_USE_TLS', False)
+    # MAIL_USE_SSL = environ.get('MAIL_USE_SSL', True)
+    # SESSION_TIMEOUT = environ.get('SESSION_TIMEOUT', 5)
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    load_dotenv(path.join(basedir, 'prod.env'))
     MYSQL_HOST = environ["MYSQL_HOST"]
     MYSQL_USER_NAME = environ["MYSQL_USER_NAME"]
     MYSQL_PASSWORD = environ["MYSQL_PASSWORD"]
@@ -27,11 +45,7 @@ class DevelopmentConfig(Config):
     MAIL_USE_TLS = environ.get('MAIL_USE_TLS', False)
     MAIL_USE_SSL = environ.get('MAIL_USE_SSL', True)
     SESSION_TIMEOUT = environ.get('SESSION_TIMEOUT', 5)
-    DEBUG = True
-
-
-class ProductionConfig(Config):
-    load_dotenv(path.join(basedir, 'prod.env'))
+    DEBUG = False
 
 
 config_data = {
