a6d8ecbe153dfd97ff525e2f74211ec4350928dc
akshaysoni001 <akshaysoni460@gmail.com>
2022-04-04 09:07:33 +0530

    port mismatched Fixed

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env/python
-
+import os
 from app import app
 from app.routes.routes import *
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5002)
+    port = int(os.environ.get('PORT', 5002))
+    app.run(host="0.0.0.0", port=port)
